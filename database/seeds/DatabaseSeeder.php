bd45b5f99b03bb6ff23bb18d300bf4bebe4d7f01
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-04-15 11:38:23 +0200

    Add call into DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            DepartmentsSeeder::class,
+            DoctorsSeeder::class,
+            MedicalServicesSeeder::class,
+            MessagesSeeder::class,
+            ReviewsSeeder::class,
+            VotesSeeder::class,
+        ]);
     }
 }

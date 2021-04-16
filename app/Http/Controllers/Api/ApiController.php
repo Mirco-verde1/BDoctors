22ecc166f78953b51d8ed1bbe76af1e30da2b2da
Mirco Verderosa <verderosamircowork@gmail.com>
2021-04-16 12:37:25 +0200

    api fixed

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
+use App\Department;
 use App\User;
 
 use App\Http\Controllers\Controller;
@@ -9,13 +11,12 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     public function __invoke()
-
     {
-
-        $doctors = User::all();
-
-
-        return response()->json($doctors);
-
+        return response()->json([
+            'success' => true,
+            'data' => User::with('departments',)->get(),
+        ], 200);
     }
+
+
 }

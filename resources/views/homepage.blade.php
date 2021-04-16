454631f2de864f0d7b93757e693c0346919da7bf
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-04-16 17:11:44 +0200

    css homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,68 +4,38 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
 
 
-        <title>Laravel</title>
+        <title>BDoctor</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
+        <div class="container-nav d-flex justify-content-center">
+          <div class="logo-img">
+             <img src="" alt="">
+          </div>
+          <div>
+              <nav class="header-nav">
+                  <ul>
+                      <li>home</li>
+                      <li>payment</li>
+                  </ul>
+              </nav>
+          </div>
+        </div>
+        <div class="jumbotron">
+          <div class="container-title-jumbotron">
+             <h1>Cerca il tuo dottore online</h1>
+             <h2>BDoctor</h2>
+          </div>
+        </div>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">

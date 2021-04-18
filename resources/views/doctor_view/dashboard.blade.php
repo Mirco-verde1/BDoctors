c8e3ed375727353082c132b6a36b9241ad4eef9a
danielpellegrini <danielpellegrini87@gmail.com>
2021-04-18 13:40:49 +0200

    home link fixed on logo

--- resources/views/doctor_view/dashboard.blade.php
+++ resources/views/doctor_view/dashboard.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
 
-                <a href="homepage" class=" btn btn-outline-success my-2 my-sm-0">{{ __('Home') }} <i class="fas fa-home"></i></a>
+                <a href="/" class=" btn btn-outline-success my-2 my-sm-0">{{ __('Home') }} <i class="fas fa-home"></i></a>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
--- resources/views/layouts/guest/partials/navbar.blade.php
+++ resources/views/layouts/guest/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-        <a href="homepage"><img src="img/logomod.png" class="logo-img" alt=""></a>
+        <a href="/"><img src="img/logomod.png" class="logo-img" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
@@ -18,7 +18,7 @@
                 <!-- Authentication Links -->
                 @if (Route::has('login'))
                 @auth
-                <a class="query-submit btn btn-outline-success my-2 my-sm-0" href="{{ url('/homepage') }}">Home</a>
+                <a class="query-submit btn btn-outline-success my-2 my-sm-0" href="{{ url('/') }}">Home</a>
                 @else
                     <a class="query-submit btn btn-outline-success my-2 my-sm-0 btn-register"
                         href="{{ route('login') }}">{{ __('Login') }}

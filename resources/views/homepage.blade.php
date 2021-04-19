0aa29d6b3770b9221a725c62d3b97410fa2c9d0a
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-04-19 09:52:42 +0200

    Auto stash before merge of "front-end" and "origin/front-end"
    modifiche carousel

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,22 +1,40 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
+        <!-- Styles -->
+         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+         <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-<title>{{ __('BDoctors - Homepage') }}</title>
+        <title>BDoctors - Home</title>
+    </head>
+    <body>
+        <div id="app">
+            @include('layouts/guest/partials/navbar')
+            <div class="container-nav d-flex justify-content-center">
+            <div>
+            </div>
+            </div>
+            <div class="jumbotron">
+            <div class="jumbotron-container-title scale-in-center">
+                <span class="title"> Cerca il tuo dottore online</span>
+                <br>
+                <span class="subtitle">BDoctors</span>
 
-@section('content')
+                <search-component></search-component>
 
-<div id="app">
-
-    <div class="container-nav d-flex justify-content-center">
-        <div>
-        </div>
-    </div>
-    <div class="jumbotron">
-        <div class="jumbotron-container-title scale-in-center">
-            <span class="title"> Cerca il tuo dottore online</span>
-            <br>
-            <span class="subtitle">BDoctors</span>
-
-            <search-component></search-component>
+            </div>
+            </div>
+            <div>
+                <h1 class="title-carousel">Dottori in evidenza </h1>
+            </div>
+            @include('layouts/guest/partials/carousel')
+            @include('layouts/guest/partials/footer')
 
         </div>
     </div>
--- resources/views/layouts/guest/partials/carousel.blade.php
+++ resources/views/layouts/guest/partials/carousel.blade.php
@@ -23,7 +23,7 @@
       <div class="carousel-item active">
 
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
                    alt="Card image cap">
@@ -36,7 +36,7 @@
             </div>
           </div>
 
-          <div class="col-md-4 clearfix d-none d-md-block">
+          <div class="col-md-3 clearfix d-none d-md-block">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
                    alt="Card image cap">
@@ -49,7 +49,7 @@
             </div>
           </div>
 
-          <div class="col-md-4 clearfix d-none d-md-block">
+          <div class="col-md-3 clearfix d-none d-md-block">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
                    alt="Card image cap">
@@ -70,7 +70,7 @@
       <div class="carousel-item">
 
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg"
                    alt="Card image cap">
@@ -83,7 +83,7 @@
             </div>
           </div>
 
-          <div class="col-md-4 clearfix d-none d-md-block">
+          <div class="col-md-3 clearfix d-none d-md-block">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg"
                    alt="Card image cap">
@@ -96,7 +96,7 @@
             </div>
           </div>
 
-          <div class="col-md-4 clearfix d-none d-md-block">
+          <div class="col-md-3 clearfix d-none d-md-block">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg"
                    alt="Card image cap">
@@ -117,7 +117,7 @@
       <div class="carousel-item">
 
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/4-col/img%20(53).jpg"
                    alt="Card image cap">
@@ -130,7 +130,7 @@
             </div>
           </div>
 
-          <div class="col-md-4 clearfix d-none d-md-block">
+          <div class="col-md-3 clearfix d-none d-md-block">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/4-col/img%20(45).jpg"
                    alt="Card image cap">
@@ -143,7 +143,7 @@
             </div>
           </div>
 
-          <div class="col-md-4 clearfix d-none d-md-block">
+          <div class="col-md-3 clearfix d-none d-md-block">
             <div class="card mb-2">
               <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/4-col/img%20(51).jpg"
                    alt="Card image cap">

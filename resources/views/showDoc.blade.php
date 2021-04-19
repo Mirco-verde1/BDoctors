53bb7eaf886e15f9eca6c3002fe2afe4cb7fcfbb
mirco verderosa <verderosamircowork@gmail.com>
2021-04-19 15:57:39 +0200

    Auto stash before merge of "backend" and "main"
    changed form in showDoc

--- resources/views/showDoc.blade.php
+++ resources/views/showDoc.blade.php
@@ -7,6 +7,29 @@
 
                 <img src="{{$user->detail->pic}}" alt="">
 
+                <h1>Dottor/ssa: {{$user->name}} {{$user->lastname}}</h1>
+
+                @foreach ($user->departments as $department)
+                    <h2>Specializzazione: {{$department->type}}</h2>
+                @endforeach
+
+                <div class="contacts">
+                    <h1>Contatti: </h1>
+                    <h2>Indirizzo: {{$user->address}}</h2>
+
+                    <h2>Email: {{$user->email}}</h2>
+
+                    <h2>{{$user->detail->phone}}</h2>
+                </div>
+
+                <div class="curriculum">
+                <h1>Curriculum: </h1>
+                <h3>{{$user->detail->curriculum}}</h3>
+                </div>
+
+                <h1>Lascia una recensione</h1>
+
+
                 <form action="{{ route('review.store', [ 'user_id' => $user->id]) }}" method="post">
                     @csrf
                     @method('POST')
@@ -37,27 +60,6 @@
 
                     <input type="submit" value="Send">
                 </form>
-
-
-                <h1>Dottor/ssa: {{$user->name}} {{$user->lastname}}</h1>
-
-                @foreach ($user->departments as $department)
-                    <h2>Specializzazione: {{$department->type}}</h2>
-                @endforeach
-
-                <div class="contacts">
-                    <h1>Contatti: </h1>
-                    <h2>Indirizzo: {{$user->address}}</h2>
-
-                    <h2>Email: {{$user->email}}</h2>
-
-                    <h2>{{$user->detail->phone}}</h2>
-                </div>
-
-                <div class="curriculum">
-                <h1>Curriculum: </h1>
-                <h3>{{$user->detail->curriculum}}</h3>
-                </div>
 
             </div>
         @endsection

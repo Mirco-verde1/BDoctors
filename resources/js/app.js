97259dcc386cd9ff12e5f0c9eb128a6cecef1df0
Salvatore Napoli <snapoli1998@gmail.com>
2021-04-20 09:58:38 +0200

    carouselComponent created

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,8 +18,6 @@
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 Vue.component('search-component', require('./components/SearchComponent.vue').default);
 
 Vue.component('advance-component', require('./components/AdvanceComponent.vue').default);
---
+++ resources/js/components/CarouselComponent.vue
@@ -0,0 +1,86 @@
+<template>
+    <!--Carousel Wrapper-->
+    <div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
+
+        <!--Controls-->
+        <div class="controls-top">
+            <a class="btn-floating" href="#multi-item-example" data-slide="prev">
+                <i class="fa fa-chevron-left"></i>
+            </a>
+            <a class="btn-floating" href="#multi-item-example" data-slide="next">
+                <i class="fa fa-chevron-right"></i>
+            </a>
+        </div>
+        <!--/.Controls-->
+
+        <!--Indicators-->
+        <ol class="carousel-indicators">
+            <!-- Creiamo tanti indicatori quante le slide -->
+            <li v-for="i in Math.ceil(results.length / cardsPerSlide)" data-target="#multi-item-example" :data-slide-to="(i - 1)" :class="(i === 1) ? 'active' : ''"></li>
+        </ol>
+        <!--/.Indicators-->
+
+        <!--Slides-->
+        <div class="carousel-inner" role="listbox">
+
+            <!-- Creiamo tante slide quanto il quoziente tra la lunghezza dell'array
+            e il numero di card in ogni slide -->
+            <div class="carousel-item" :class="(i === 1) ? 'active' : ''" v-for="i in Math.ceil(results.length / cardsPerSlide)">
+
+                <div class="row container d-flex flex-row p-2 flex-wrap">
+                    <div class="card col-md-3 p-2 bd-highlight doctor-card" :class="(i === 1) ? 'clearfix d-none d-md-block' : ''" v-for="doctor in carouselLoop(i, results)">
+                        <div class="img-container">
+                            <img class="card-img-top" :src="doctor.detail.pic" alt="Card image cap">
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title">{{doctor.name}} {{doctor.lastname}}</h4>
+                            <h5 class="card-text">Specializzazioni:</h5>
+                            <h6 class="card-text" v-for="department in doctor.departments">{{department.type}}</h6>
+                            <a :href="'doctor/' + doctor.id" class="query-submit btn btn-outline-success my-2 my-sm-0 btn-register">Info</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/.Slides-->
+
+    </div>
+    <!--/.Carousel Wrapper-->
+</template>
+
+<script>
+export default {
+
+    data: function () {
+
+        return {
+            results: JSON.parse(localStorage.getItem('results')),
+            cardsPerSlide: 3
+        }
+    },
+
+    methods: {
+
+        // Scomponiamo l'array di risultati per recuperare di volta in volta i valori con cui popolare le slide
+        carouselLoop: function(index, array) {
+            const limit = this.cardsPerSlide;
+            let start, end;
+
+            end = index * limit;
+            start = end - limit;
+
+            return array.slice(start, end);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .img-container {
+        width: 300px;
+    }
+
+    .card-img-top {
+        width: 100%;
+    }
+</style>

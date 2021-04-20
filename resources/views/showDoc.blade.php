a3b7293b652a499e0f8e514369d5e092b265d0c1
Daniel <danielpellegrini87@gmail.com>
2021-04-20 14:02:18 +0200

    Doctor layout completed

--- resources/views/showDoc.blade.php
+++ resources/views/showDoc.blade.php
@@ -25,7 +25,6 @@
 
                         {{-- box con shadow --}}
                         <div class="box-general">
-
                             <div class="profile">
 
                                 <div class="row">
@@ -58,71 +57,122 @@
                                         @endforeach
 
 
-
-
-
-
-
                                         {{-------------------------------------------- qui inserire medical services --------------------------------------------}}
 
                                     </div>
 
                                 </div>
 
+                            </div>
+                        </div>
+
+
+                        {{--************ CONTATTI ************--}}
+
+                        <nav id="secondary-nav" class="margin-top-container">
+                            <div>
+                                <ul class="justify-content-center">
+                                    <li>
+                                        <b class="active-nav">Contatti</b>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+
+                        <div class="box-general">
+                            <div class="profile">
+
+                                <div class="row">
+                                    <div class="contacts">
+                                        <h2>Indirizzo: {{$user->address}}</h2>
+
+                                        <h2>Email: {{$user->email}}</h2>
+
+                                        <h2>{{$user->detail->phone}}</h2>
+                                    </div>
+
+                                </div>
 
                             </div>
+                        </div>
 
+
+                        {{--************ CURRICULUM ************--}}
+
+                        <nav id="secondary-nav" class="margin-top-container">
+                            <div>
+                                <ul class="justify-content-center">
+                                    <li>
+                                        <b class="active-nav">Curriculum</b>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+
+                        <div class="box-general">
+                            <div class="profile">
+                                <div class="row">
+                                    <div class="curriculum">
+                                    <h3>{{$user->detail->curriculum}}</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {{--************ RECENSIONE ************--}}
+
+                        <nav id="secondary-nav" class="margin-top-container">
+                            <div>
+                                <ul class="justify-content-center">
+                                    <li>
+                                        <h1 class="active-nav">Lascia una recensione</h1>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+
+                        <div class="box-general">
+                            <div class="profile">
+                                <div class="">
+                                    <form action="{{ route('review.store', [ 'user_id' => $user->id]) }}" method="post">
+                                        @csrf
+                                        @method('POST')
+
+                                        <div class="form-group position-relative">
+                                            <label for="name" class="form-label">Name</label>
+                                            <input class="form-control {{ $errors->has('name') ? 'is-invalid' : ''}}" type="text" name="name" placeholder="Name">
+                                            <div class="invalid-tooltip">
+                                                {{ $errors->first('name') }}
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group position-relative">
+                                            <label for="email">Email</label>
+                                            <input class="form-control {{ $errors->has('email') ? 'is-invalid' : ''}}" type="text" name="email" placeholder="Email">
+                                            <div class="invalid-tooltip">
+                                                {{ $errors->first('email') }}
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group position-relative">
+                                            <label for="body">Body</label>
+                                            <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}}" type="text" name="body" placeholder="Body" rows="6"></textarea>
+                                            <div class="invalid-tooltip">
+                                                {{ $errors->first('body') }}
+                                            </div>
+                                        </div>
+
+                                        <input type="submit" value="Send">
+                                    </form>
+                                </div>
+                            </div>
                         </div>
 
 
 
-                        <div class="contacts">
-                            <h1>Contatti: </h1>
-                            <h2>Indirizzo: {{$user->address}}</h2>
-
-                            <h2>Email: {{$user->email}}</h2>
-
-                            <h2>{{$user->detail->phone}}</h2>
-                        </div>
-
-                        <div class="curriculum">
-                        <h1>Curriculum: </h1>
-                        <h3>{{$user->detail->curriculum}}</h3>
-                        </div>
-
-                        <h1>Lascia una recensione</h1>
 
 
-                        <form action="{{ route('review.store', [ 'user_id' => $user->id]) }}" method="post">
-                            @csrf
-                            @method('POST')
 
-                            <div class="form-group position-relative">
-                                <label for="name" class="form-label">Name</label>
-                                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : ''}}" type="text" name="name" placeholder="Name">
-                                <div class="invalid-tooltip">
-                                    {{ $errors->first('name') }}
-                                </div>
-                            </div>
-
-                            <div class="form-group position-relative">
-                                <label for="email">Email</label>
-                                <input class="form-control {{ $errors->has('email') ? 'is-invalid' : ''}}" type="text" name="email" placeholder="Email">
-                                <div class="invalid-tooltip">
-                                    {{ $errors->first('email') }}
-                                </div>
-                            </div>
-
-                            <div class="form-group position-relative">
-                                <label for="body">Body</label>
-                                <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}}" type="text" name="body" placeholder="Body" rows="6"></textarea>
-                                <div class="invalid-tooltip">
-                                    {{ $errors->first('body') }}
-                                </div>
-                            </div>
-
-                            <input type="submit" value="Send">
-                        </form>
                     </div>
 
                 </div>

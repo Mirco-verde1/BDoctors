67c3fb5b5ee680a5f354cf82fd081a4540cd2a44
Daniel <danielpellegrini87@gmail.com>
2021-04-20 17:52:08 +0200

    fixed images into dashboard

--- resources/views/doctor_view/dashboard.blade.php
+++ resources/views/doctor_view/dashboard.blade.php
@@ -5,8 +5,8 @@
 <div class="container-fluid">
   <div class="row">
 
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar margin-top-container navbar-expand-md">
-      <div class="sidebar-sticky margin-top-container">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar  navbar-expand-md">
+      <div class="sidebar-sticky ">
 
         <figure class="doctor-pic-dashboard-container ">
             <img class="doctor-pic" src="{{ Auth::user()->getPic()->pic }}" alt="{{ Auth::user()->name }} {{ Auth::user()->lastname }}">

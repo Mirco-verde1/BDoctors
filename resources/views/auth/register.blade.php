9142cd8f72f05580bd746b0af62fefea5ce6e4fe
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-04-20 12:05:52 +0200

    change font and border radius carousel

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -117,7 +117,7 @@
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="query-submit btn btn-outline-success my-2 my-sm-0 d-inline">
                                         {{ __('Register') }}
                                     </button>
                                 </div>

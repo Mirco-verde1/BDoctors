9278df86769805fb60134349b55e3658306da1cb
Mirco Verderosa <verderosamircowork@gmail.com>
2021-04-21 17:08:52 +0200

    +

---
+++ app/Http/Controllers/PaymentsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+use Braintree\Transaction as Braintree_Transaction;
+
+class PaymentsController extends Controller
+{
+    public function make(Request $request)
+
+{
+
+    $payload = $request->input('payload', false);
+    $payment_method_nonce = $request->get('payment_method_nonce');
+    $status = Braintree_Transaction::sale([
+                            'amount' => '10.00',
+                            'paymentMethodNonce' => $payment_method_nonce,
+                            'options' => [
+                                       'submitForSettlement' => True
+                                         ]
+              ]);
+
+    return view('layouts.guest/partials.checkout',compact('status'));
+
+}
+}
--- app/Sponsor.php
+++ app/Sponsor.php
@@ -5,7 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Sponsor extends Model
+
 {
+
+    protected $fillable = [ 'type', 'duration', 'price', 'user_id'];
+
     public function users() {
         return $this->belongsToMany(User::class);
     }

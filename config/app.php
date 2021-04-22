080bbb07b5b6f7731aeb2f10f5fd8a99663dc579
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-04-22 12:48:26 +0200

    Add MarckDown on curriculum and other graphic feat

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
 
         /*
          * Package Service Providers...

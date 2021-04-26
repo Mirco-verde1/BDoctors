0c2d20425cfddcb9986450ecf6eea2b7ea932e3f
Daniel <danielpellegrini87@gmail.com>
2021-04-26 14:12:27 +0200

    checkout cards style updated

--- resources/views/layouts/guest/partials/checkout.blade.php
+++ resources/views/layouts/guest/partials/checkout.blade.php
@@ -11,16 +11,21 @@
             @csrf
         @method('POST')
         <div class="pricing-header text-center">
-            <h1 class="title-checkout">Scelga la sua sponsorizzazione:</h1>
+            <h1 class="title-checkout">Scegli la tua sponsorizzazione:</h1>
           </div>
             <div class="card-deck mb-3 text-center">
 
 
                 @foreach ($sponsors as $sponsor)
-              <div class="card mb-4 box-shadow">
-                <div class="card-header">
-                  <h4 class="my-0 font-weight-normal">{{$sponsor->type}}</h4>
-                </div>
+
+
+                <div class="card mb-4 box-shadow">
+                    <div class="card-header {{($sponsor->type === 'Standard') ? 'standard-plain' : ($sponsor->type === 'Premium' ? 'premium-plain' : 'pro-plain')}}">
+                        <h4 class="my-0 font-weight-normal">{{$sponsor->type}}</h4>
+                    </div>
+
+
+
                 <div class="card-body">
                   <h1 class="card-title pricing-card-title">{{$sponsor->price}}€ <small class="text-muted">/ {{$sponsor->duration}}h</small></h1>
                   <ul class="list-unstyled mt-3 mb-4">
@@ -31,7 +36,7 @@
                 </div>
                 <span class="style-radio" role="group" aria-label="Basic radio toggle button group">
                     <input type="radio" class="btn-check" name="sponsor" id="sponsor{{$sponsor->id}}" value="{{$sponsor->id}}" autocomplete="off" >
-                    <label class="btn btn-outline-primary checkmark " for="sponsor{{$sponsor->id}}">{{$sponsor->type}}</label>
+                    <label class="btn btn-outline{{($sponsor->type === 'Standard') ? '-primary' : ($sponsor->type === 'Premium' ? '-success' : '-warning')}} {{($sponsor->type === 'Standard') ? 'checkmark-standard' : ($sponsor->type === 'Premium' ? 'checkmark-premium' : 'checkmark-pro')}} " for="sponsor{{$sponsor->id}}">{{$sponsor->type}}</label>
                   </span>
               </div>
               @endforeach
